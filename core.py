5660cd99ab44850e5f6d0c7fbf44176958f10127
Mycca777 <55271744+Mycca777@users.noreply.github.com>
2019-11-14 21:01:27 +0300

    Update core.py

--- core.py
+++ core.py
@@ -1,9 +1,14 @@
-import requests
+import requests as req
 from bs4 import BeautifulSoup
 class CinemaParser:
     def __init__(self, city):
         self.city = city
-
+    def extract_raw_content(self, content):
+        content = self.content
+        resp = req.get("https://spb.subscity.ru")
+        content = BeautifulSoup(resp.text, 'lxml')
+    def print_raw_content(self, content):
+        return (extract_raw_content(self, content).prettify())
 
 
 
