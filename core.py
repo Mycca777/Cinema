ff222037ffddf73419489c5c8a9cec559e3933a7
Mycca777 <55271744+Mycca777@users.noreply.github.com>
2019-11-15 11:34:02 +0300

    Update core.py

--- core.py
+++ core.py
@@ -1,19 +1,38 @@
 import requests as req
 from bs4 import BeautifulSoup
 class CinemaParser:
-    def __init__(self, city):
+    def __init__(self, city='msk'):
         self.city = city
     def extract_raw_content(self):
         resp = req.get("https://spb.subscity.ru")
-        content = BeautifulSoup(resp.text, 'html.parser')
+        self.content = BeautifulSoup(resp.text, 'html.parser')
+
     def print_raw_content(self):
         self.extract_raw_content()
-        return (extract_raw_content(self, content).prettify())
-    def  get_films_list():
-        soup = self.extract_raw_content()
-        soup.find("span", id="mylist", )
+        print(self.content.prettify())
+    def  get_films_list(self):
+        self.extract_raw_content()
+        films = self.content.find_all("div", {"class" : "movie-plate"})
+        titles = []
+        for film in films:
+            titles.append(film["attr-title"])
+        return titles
+    def get_film_nearest_session(self, film_name):
+        url = "https://spb.subscity.ru"
+        self.extract_raw_content()
+        href = ''
+        films = self.content.find_all("div", {"class" : "movie-plate"})
+        for film in films:
+            if film == film_name:
+                title = films.find_all("div", {"class" : "movie-title"})
+                a = title.find_all("a", {"class" : "underdashed"})
+                href = a["href"] + url
+                return href
 
 
 spb_parser = CinemaParser('spb')
-msk_parser = CinemaParser()
-another_msk_parser = CinemaParser('msk')
+#spb_parser.print_raw_content()
+print(spb_parser.get_films_list())
+msk_parser = CinemaParser('msk')
+print(spb_parser.get_film_nearest_session('Джокер'))
+#print(another_msk_parser = CinemaParser('msk'))
