8a909fd6b2c8fa678ef99594ab7127ff3b46dada
Mycca777 <55271744+Mycca777@users.noreply.github.com>
2019-11-22 09:14:23 +0300

    Update example.py

--- example.py
+++ example.py
@@ -1,1 +1,7 @@
-print(5)
+from cinemaparse import CinemaParse
+spb_parser = CinemaParser('spb')
+#spb_parser.print_raw_content()
+print(spb_parser.get_films_list())
+msk_parser = CinemaParser('msk')
+print(spb_parser.get_film_nearest_session('Джокер'))
+#print(another_msk_parser = CinemaParser('msk'))
