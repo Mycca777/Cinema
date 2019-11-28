93002dc9102259e5d024e52fbdbf83cae607c1e7
Mycca777 <55271744+Mycca777@users.noreply.github.com>
2019-11-28 21:01:55 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -60,3 +60,4 @@ print(spb_parser.get_films_list())
 # msk_parser = CinemaParser('msk')
 print(spb_parser.get_film_nearest_session('Ford против Ferrari'))
 #print(another_msk_parser = CinemaParser('msk'))
+#
