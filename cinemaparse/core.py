b2d87dc0231e4e539ea8648624674304e803bd82
Mycca777 <55271744+Mycca777@users.noreply.github.com>
2019-11-29 20:34:23 +0300

    Update core.py

--- cinemaparse/core.py
+++ cinemaparse/core.py
@@ -4,8 +4,11 @@ from datetime import datetime
 class CinemaParser:
     def __init__(self, city='msk'):
         self.city = city
-        self.url = 'http://' + self.city + '.subscity.ru'
-        self.content = None
+        if self.city == 'msk':
+            self.url = 'https://msk.subscity.ru/'
+        else:
+            self.url = 'https://spb.subscity.ru/'
+        self.content = self.extract_raw_content(self.url)
     def extract_raw_content(self, url):
         resp = req.get(url)
         self.content = BeautifulSoup(resp.text, 'html.parser')
@@ -21,37 +24,54 @@ class CinemaParser:
             titles.append(film["attr-title"])
         return titles
     def get_film_nearest_session(self, film_name):
+        film_name = film_name.strip(' ')
+        film_name = film_name.lower()
         if not self.content:
             self.extract_raw_content(self.url)
+        FILMS = self.get_films_list()
+        for i in range(len(FILMS)):
+            if film_name == FILMS[i].lower():
+                continue
         self.extract_raw_content(self.url)
         films = self.content.find_all("div", {"class" : "movie-plate"})
-        spans = self.content.find_all("span", {"class" : "label label-bg label-default normal-font"})
-        print(spans)
-        print(len(films))
         for film in films:
-            if film["attr-title"] != film_name:
-                continue
+          if str(film["attr-title"]).lower() == film_name.lower():
             a = film.find("a", {"class" : "underdashed"})
-            print(a)                 
             href = a["href"]
             href_for_films = self.url + "/" + href
-            print(href_for_films)
-        result = []
-        self.extract_raw_content(href_for_films)
-        times = self.content.find("table", {"class" : "table table-bordered table-condensed table-curved table-striped table-no-inside-borders"})
-        print(times)
-        for time in times:
-            print(time)
-            td = time.find("td", {"class" : "text-center cell-screenings"})
-            print(td)
-            res = int(td["attr-time"])
-            result.append(res)
-        resultat = min(result)
-        #if resultat // 3600 // 24 // 365
-        return resultat//3600//24//365
+            self.extract_raw_content(href_for_films)
+            span = film.find("span", {"class": "label label-bg label-default normal-font"})
+            times = self.content.find_all("table", {"class" : "table table-bordered table-condensed table-curved table-striped table-no-inside-borders"})
+            for time in times:
+                trs = self.content.find_all("tr", {"class": "row-entity"})
+                thing_for_day1 = film.find_all('div', {'class': 'movie-plate-table'})
+                thing_for_day2 = thing_for_day1[0].find_all('div', {'class': 'movie-plate-row'})
+                day = thing_for_day2[1].find_all('span', {'class': 'label label-bg label-default normal-font'})
+                day = day[0].text
+                if day != ' сегодня':
+                    korteh = 'None', 'None'
+                elif day == ' сегодня':
+                        typok = times[0]
+                        times_for_result = typok.find_all('tr', {'class': 'row-entity'})
+                        result = []
+                        for i in range(len(times_for_result)):
+                            n = times_for_result[i].find_all('td', {'class': 'col-sm-8 col-xs-1'})
+                            resultat = n[0].find_all('td')
+                            res = resultat[0]['attr-time']
+                            result.append(int(res))
+                        closest_time_index = result.index(min(result))
+                        route = times_for_result[closest_time_index].find_all('td', {'class': 'col-sm-8 col-xs-1'})
+                        normal_time = route[0].find_all('a')[0]
+                        thing_for_cinema = times_for_result[closest_time_index].find_all('td', {'class': 'col-sm-4 col-xs-11'})
+                        cinema = thing_for_cinema[0].find_all('a')[0]
+                        korteh = cinema.text, normal_time.text
+
+                        return korteh
+    def get_soonest_session(self, film_name):
+        respect
+ 
 spb_parser = CinemaParser('spb')
-#spb_parser.print_raw_content()
+spb_parser.print_raw_content()
 print(spb_parser.get_films_list())
-# msk_parser = CinemaParser('msk')
-print(spb_parser.get_film_nearest_session('Ford против Ferrari'))
-#print(another_msk_parser = CinemaParser('msk'))
+msk_parser = CinemaParser('msk')
+print(spb_parser.get_film_nearest_session('Прекрасная эпоха    '))
